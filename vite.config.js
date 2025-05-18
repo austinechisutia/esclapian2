064857dcce3d895fed56ade36315e690f1e719c4
Austine <austineakhonya624@gmail.com>
2025-05-18 11:37:24 +0300

    chore: update cofiguration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/esclapian2/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
